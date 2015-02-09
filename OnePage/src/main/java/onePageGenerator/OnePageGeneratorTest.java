328c718821f6c19914098222bc9f8852d3bc1b72
Atrocyte <mendeltl@hotmail.com>
2015-02-09 15:48:34 +0100

    Added skeleton unit test

---
+++ OnePage/src/main/java/onePageGenerator/OnePageGeneratorTest.java
@@ -0,0 +1,16 @@
+package onePageGenerator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OnePageGeneratorTest {
+
+	@Test
+	public void testMain() {
+		OnePageGenerator sut = new OnePageGenerator();
+		
+		fail("Not yet implemented");
+	}
+
+}
